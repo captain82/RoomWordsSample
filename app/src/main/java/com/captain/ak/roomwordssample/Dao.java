e737a8735bdf0ef814f85997b65dd6d88da48f82
Ankit Kumar <nktkumar82@gmail.com>
2019-05-03 00:09:19 +0530

    Live data added

--- app/src/main/java/com/captain/ak/roomwordssample/Dao.java
+++ app/src/main/java/com/captain/ak/roomwordssample/Dao.java
@@ -1,5 +1,6 @@
 package com.captain.ak.roomwordssample;
 
+import android.arch.lifecycle.LiveData;
 import android.arch.persistence.room.Insert;
 import android.arch.persistence.room.Query;
 
@@ -15,7 +16,7 @@ public interface Dao {
     void deleteAll();
 
     @Query("SELECT * from WORD_TABLE ORDER BY word ASC")
-    List<Word> getAllWords();
+    LiveData<List<Word>> getAllWords();
 
 
 
